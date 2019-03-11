5e81e4cdab21e66b8d18abdc87bd2f47c0dd3eeb
SBerk97 <sberk@bradford.ac.uk>
2019-03-11 18:04:23 +0000

    Added constructor and get/set to Contract

--- src/java/Contract.java
+++ src/java/Contract.java
@@ -1,2 +1,24 @@
+import java.util.List;
+
 public class Contract {
+
+    private float cost;
+    private int maxNumberOfDesigners;
+    List<Employee> designersTeam;
+
+    public Contract(float cost, int maxNumberOfDesigners) {
+        this.cost = cost;
+        this.maxNumberOfDesigners = maxNumberOfDesigners;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void addDesigner(Employee designer) {
+        if(designersTeam.size() < maxNumberOfDesigners) {
+
+            designer.increaseNumberOfContracts();
+        }
+    }
 }
